6f3dc19ecfa35c7489329c1770929a90f6c9b354
tom_glb <goodMorning_glb@xej.com>
2020-05-25 15:01:01 +0800

    完善了修改

--- src/main/java/cn/xej/controller/TestController.java
+++ src/main/java/cn/xej/controller/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-    @RequestMapping("/test")
-    public String test(){
-        return "test";
+    @RequestMapping("/test1")
+    public String test1(){
+        return "test1";
     }
 }
